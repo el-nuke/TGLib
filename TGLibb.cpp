e685da650a979be138cac25b347d40526000a5cb
el-nuke <villalobenja02@gmail.com>
2023-12-14 20:20:59 -0300

    assad

--- TGLibb.cpp
+++ TGLibb.cpp
@@ -3,6 +3,37 @@
 
 using namespace std;
 
+struct resultados
+{
+    string tipodecarrera;
+    chrono::microseconds selection, bubble, insertion, shell, merge, quick, heap;
+    bool selectionW, bubbleW, insertionW, shellW, mergeW, quickW, heapW;
+
+    void reset() {
+        chrono::microseconds zeroms = chrono::microseconds(0);
+        selection = bubble = insertion = shell = merge = quick = heap = zeroms;
+        selectionW = bubbleW = insertionW = shellW = mergeW = quickW = heapW = false;
+    }
+
+    void printAll(){
+        cout << "\n-- RESULTADOS --" << endl;
+        cout << "Selection: " << selection.count() << " microsecs" << endl;
+        cout << "Bubble: " << bubble.count() << " microsecs" << endl;
+        cout << "Insertion: " << insertion.count() << " microsecs" << endl;
+        cout << "Shell: " << shell.count() << " microsecs" << endl;
+        cout << "Merge: " << merge.count() << " microsecs" << endl;
+        cout << "Quick: " << quick.count() << " microsecs" << endl;
+        cout << "Heap: " << heap.count() << " microsecs" << endl;
+    }
+};
+
+resultados* crearResultados(){
+    resultados* ts = new resultados();
+    ts->reset();
+    return ts;
+}
+
+
 void printArray(int inArray[], int size){
     for (int c=0;c<size;c++){
         cout << inArray[c] << " ";
@@ -12,12 +43,13 @@ void printArray(int inArray[], int size){
 }
 
 //Busca el valor mas pequeño desde el principio y lo reubica al principio, avanzando hasta reubicar todo el arreglo
+//- *sortedarray = selectionSort(array, size)
 int* selectionSort(int inArray[], int size){
     for (int i=0; i<size; i++){
         for (int j=i+1; j<size; j++){
             if (inArray[j] < inArray[i]){
                 int temp = inArray[i];
-                inArray[i], inArray[j];
+                inArray[i] = inArray[j];
                 inArray[j] = temp;
             }
         }
@@ -25,7 +57,8 @@ int* selectionSort(int inArray[], int size){
     return inArray;
 }
 
-//Avanza por el arreglo por pares, reubicando al menor del par a la izq. y al mayor a la der.
+//Avanza por el arreglo por pares, reubicando al menor del par a la izq. del par y al mayor a la der. del par
+//- *sortedarray = bubbleSort(array, size)
 int* bubbleSort(int inArray[], int size){
     for (int i=1; i<size; i++){
         for (int j=0; j<size-1; j++){
@@ -40,6 +73,7 @@ int* bubbleSort(int inArray[], int size){
 }
 
 //Avanza por el arreglo desde el principio, ordenando cada valor a medida que lo recorre
+//- *sortedarray = insertionSort(array, size)
 int* insertionSort(int inArray[], int size){
     for (int i=1; i<=size-1; i++){
         int j = i;
@@ -56,6 +90,7 @@ int* insertionSort(int inArray[], int size){
 
 //Similar a insertion, pero empieza usando un gap de n, con el cual compara y ordena valores adyacentes en n espacios, el gap se 
 //reduce con cada iteración hasta 1, donde itera por una ultima vez de forma idéntica a insertion.
+//- *sortedarray = shellSort(array, size)
 int* shellSort(int inArray[], int size){
     for (int gap = size/2; gap>0; gap/=2){
         for (int i=gap; i<size; i++){
@@ -111,7 +146,7 @@ void merge(int arr[], int left, int mid, int right) {
 }
 
 //Divide el arreglo en mitades cada vez mas pequeñas, luego los junta mitad por mitad mientras los ordena hasta volver al tamaño del arreglo original
-//- mergesort(array, 0, size-1)
+//- *sortedarray = mergesort(array, 0, size-1)
 int* mergeSort(int arr[], int left, int right) {
     if (left < right) {
         int mid = left + (right - left) / 2;
@@ -181,6 +216,8 @@ void heapify(int inArray[], int n, int i){
     }
 }
 
+//Usa logica de arboles binarios para reordenar arreglos
+//- heapSort(array, size)
 void heapSort(int inArray[], int size){
     int s = size;
     
@@ -197,38 +234,119 @@ void heapSort(int inArray[], int size){
     }
 }
 
+void carrera(int inArray[], int inSize){
+    resultados *res = crearResultados();
+    int size = inSize;
+    int* sortedarray;
+    int array[size];
+    
+    for (int c=0; c<7; c++){
+        for (int i=0; i<size; i++){
+            array[i] = inArray[i];
+        }
+                
+        auto tStart = chrono::high_resolution_clock::now();
+        auto tStop = chrono::high_resolution_clock::now();
+        
+        
+        switch (c)
+        {
+        case 0:
+            cout << "testing selection" << endl;
+            tStart = chrono::high_resolution_clock::now();
+            sortedarray = selectionSort(array, size);
+            tStop = chrono::high_resolution_clock::now();
+            //cout << "-- Timer stop --" << endl;
+            res->selection = chrono::duration_cast<chrono::microseconds>(tStop - tStart);
+            break;
+
+        case 1:
+            cout << "testing bubble" << endl;
+            tStart = chrono::high_resolution_clock::now();
+            sortedarray = bubbleSort(array, size);
+            tStop = chrono::high_resolution_clock::now();
+            //cout << "-- Timer stop --" << endl;
+            res->bubble = chrono::duration_cast<chrono::microseconds>(tStop - tStart);
+            break;
+
+        case 2:
+            cout << "testing insertion" << endl;
+            tStart = chrono::high_resolution_clock::now();
+            sortedarray = insertionSort(array, size);
+            tStop = chrono::high_resolution_clock::now();
+            //cout << "-- Timer stop --" << endl;
+            res->insertion = chrono::duration_cast<chrono::microseconds>(tStop - tStart);
+            break;
+        
+        case 3:
+            cout << "testing shell" << endl;
+            tStart = chrono::high_resolution_clock::now();
+            sortedarray = shellSort(array, size);
+            tStop = chrono::high_resolution_clock::now();
+            //cout << "-- Timer stop --" << endl;
+            res->shell = chrono::duration_cast<chrono::microseconds>(tStop - tStart);
+            break;
+        
+        case 4:
+            cout << "testing merge" << endl;
+            tStart = chrono::high_resolution_clock::now();
+            mergeSort(array, 0, size-1);
+            tStop = chrono::high_resolution_clock::now();
+            //cout << "-- Timer stop --" << endl;
+            res->merge = chrono::duration_cast<chrono::microseconds>(tStop - tStart);
+            break;
+
+        case 5:
+            cout << "testing quick" << endl;
+            tStart = chrono::high_resolution_clock::now();
+            quickSort(array, 0, size-1);
+            tStop = chrono::high_resolution_clock::now();
+            //cout << "-- Timer stop --" << endl;
+            res->quick = chrono::duration_cast<chrono::microseconds>(tStop - tStart);
+            break;
+        
+        case 6:
+            cout << "testing heap" << endl;
+            tStart = chrono::high_resolution_clock::now();
+            heapSort(array, size);
+            tStop = chrono::high_resolution_clock::now();
+            //cout << "-- Timer stop --" << endl;
+            res->heap = chrono::duration_cast<chrono::microseconds>(tStop - tStart);
+            break;
+        
+        default:
+            break;
+        }
+        printArray(array, size);
+    }
+    
+    
+    res->printAll();
+}
+
 int main(){
-    int size = 500;
-    int myArray[size];
-    for (int c=0;c<size;c++){
-        myArray[c] = rand()%100;
+    srand(time(0));
+
+    //int size = rand()%(110000 - 100000 + 1) + 100000;
+    int size = 10;
+    int arreglo[size];
+
+   //Llena el array ordenado
+    for (int i=0; i<size; i++){
+        arreglo[i] = i+100;
+    }
+    //Reordena aleatoriamente n veces
+    for (int i=0; i<10000; i++){
+        int x = rand()%size;
+        int y = rand()%size;
+        int temp = arreglo[x];
+        arreglo[x] = arreglo[y];
+        arreglo[y] = temp;  
     }
 
+    carrera(arreglo, size);
 
-    cout << "Original array: ";
-    printArray(myArray, size);
-    
-
-    //EMPIEZA TIMER -------------------------------------------------------------------------------------------------
-    auto tStart = chrono::high_resolution_clock::now();
-    cout << "-- Timer start --" << endl;
-    //---------------------------------------------------------------------------------------------------------------
-    
-    //int* sorted = shellSort(myArray, size);
-    quickSort(myArray, 0, size-1);
-
-    //DETIENE TIMER Y GUARDA EL TIEMPO TRANSCURRIDO ------------------------------------------------------------------
-    auto tStop = chrono::high_resolution_clock::now();
-    cout << "-- Timer stop --" << endl;
-    auto timeTaken = chrono::duration_cast<chrono::milliseconds>(tStop - tStart);
-    //---------------------------------------------------------------------------------------------------------------
-
-    cout << "Sorted array: ";
-    printArray(myArray, size);
-    
-
-    //prints time elapsed
-    cout << "Time elapsed: " << timeTaken.count() << " ms" << endl;
+    //tiempos->printAll();
 
     return 0;
 }
